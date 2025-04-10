6cbf969ecff60b7c6dd0b843ab875d6df05995b7
jlopezmoliner <j.lopezmoliner@ub.edu>
2025-04-10 08:45:45 +0200

    small changes

--- R/functions.R
+++ R/functions.R
@@ -14,6 +14,19 @@ PosVel.op <- function(p, x, A= matrix(c(1,0,1/90,1),2,2), C = diag(1,2,2), V=dia
   
 }
 
+lqgRW.op <- function(p,x,dyn,actor,sigma_cursor=1)
+{
+  
+  W <- diag(c(sigma_target,sigma_cursor),2,2)
+  W[1,1] <- p # p[1] p[2]
+  #W[2,2] <- p[2]
+  dyn$W <- W
+  actor$W <- W
+  #  res <- -K_logLik(x=x,A = A,C = C, V=V, W=W)$loglik
+  res <- -lqg(x,dyn,actor)$loglik
+}
+
+
 dxdt <- function(x,dt=1)
 {
   
@@ -21,31 +34,23 @@ dxdt <- function(x,dt=1)
   
 }
 
-correlogram <- function(x,maxlag)
+correlogram <- function(x,maxlag,deriv)
 {
-  
+  if (deriv) {
+    x <- apply(x, 2, diff)
+  }
  res <- ccf(x[,2],x[,1],plot=F,lag.max = maxlag,na.action = na.contiguous)
- res <- data.frame(lag=as.numeric(res$lag),step=as.numeric(res$lag),ccf=as.numeric(res$acf))
+ res <- data.table(lag=as.numeric(res$lag),ccf=as.numeric(res$acf))
  res
 }
 
-compute_ccf <- function(x,maxlag=20)
+compute_ccf <- function(x,maxlag=100,deriv=TRUE)
 {
   ntrials <- dim(x)[2]
-  res <- apply(x,2,correlogram,maxlag=maxlag)
+  res <- apply(x,2,correlogram,maxlag=maxlag,deriv=deriv)
   res2 <- do.call(rbind,res)
   res2$trial <- rep(1:ntrials,each=maxlag*2+1)
   res2
 }
 
 
-lqgRW.op <- function(p,x,dyn,actor,sigma_cursor=1)
-{
-  
-  W <- diag(c(sigma_target,sigma_cursor),2,2)
-  W[1,1] <- p
-  dyn$W <- W
-  actor$W <- W
-  #  res <- -K_logLik(x=x,A = A,C = C, V=V, W=W)$loglik
-  res <- -lqg(x,dyn,actor)$loglik
-}
